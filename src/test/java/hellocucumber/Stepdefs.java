59dc4f6c673a08a68fb03e7ec13ea047b85e94c4
VicRen <vic.ren@me.com>
2018-07-19 13:07:34 +0800

    尝试 And

--- src/test/java/hellocucumber/Stepdefs.java
+++ src/test/java/hellocucumber/Stepdefs.java
@@ -1,6 +1,7 @@
 package hellocucumber;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
@@ -13,14 +14,22 @@ class IsItFriday {
         }
         return "不是";
     }
+
+    static String isItBlackFriday(String weekday, int date) {
+        if (isItFriday(weekday).equals("是的") && date == 13) {
+            return "是的";
+        }
+        return "不是";
+    }
 }
 
 public class Stepdefs {
     private String today;
+    private int date;
     private String actualAnswer;
 
     @Given("^今天是 \"([^\"]*)\"$")
-    public void today_is(String someday) throws Exception {
+    public void today_is(String someday) {
         this.today = someday;
     }
 
@@ -31,6 +40,26 @@ public class Stepdefs {
 
     @Then("^我应该被告知 \"([^\"]*)\"$")
     public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Given("^今天是星期五$")
+    public void 我问今天是星期五() {
+       this.today = "星期五";
+    }
+
+    @And("^今天是(\\d+)日$")
+    public void 今天是日(int date) {
+        this.date = date;
+    }
+
+    @When("^我提问今天是黑色星期五么？$")
+    public void 我提问今天是黑色星期五么() {
+        this.actualAnswer = IsItFriday.isItBlackFriday(today, date);
+    }
+
+    @Then("^我被告知 \"([^\"]*)\"$")
+    public void 我被告知是的(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
     }
 
